a246bfac879f704e11a335e17d39e9ecc92c57b1
100522187 <100522187@alumnos.uc3m.es>
2025-12-14 16:16:10 +0100

    formulario de compra

--- script/local_storage_ops.js
+++ script/local_storage_ops.js
@@ -16,3 +16,34 @@ function getMyInfo() {
     let email = localStorage.getItem(EMAIL_LS_DATA);
     return !email ? null : getRegisteredUsers().get(email)
 }
+
+const BOOKINGS_LS_DATA = "historial_compras"; 
+
+/**
+ * Guarda una nueva reserva en el historial general
+ * @param {Object} nuevaReserva
+ */
+function guardarReserva(nuevaReserva) {
+    // Obtenemos lo que ya hay guardado
+    let historial = JSON.parse(localStorage.getItem(BOOKINGS_LS_DATA)) || [];
+    
+    // Añadimos la nueva reserva a la lista
+    historial.push(nuevaReserva);
+    
+    // Guardamos la lista actualizada
+    localStorage.setItem(BOOKINGS_LS_DATA, JSON.stringify(historial));
+    console.log("Reserva guardada en Storage:", nuevaReserva);
+}
+
+
+//Obtiene las reservas solo del usuario actual
+ 
+function getMisReservas() {
+    const user = getMyInfo();
+    if (!user) return [];
+
+    let historial = JSON.parse(localStorage.getItem(BOOKINGS_LS_DATA)) || [];
+    
+    // Filtramos para devolver solo las compras de este email
+    return historial.filter(reserva => reserva.usuario === user.email);
+}
